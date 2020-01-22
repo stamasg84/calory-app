d25ae288f139e103077f9f3bcd339a58505532ea
Tamás Selmeczi <stamasg84@gmail.com>
2020-01-22 10:10:45 +0100

    Scaffolding

---
+++ CaloryAPI/CaloryAPI/Controllers/MealsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaloryAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MealsController : ControllerBase
+    {      
+        public MealsController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IEnumerable<Meal> Get()
+        {
+            return new[] { new Meal() };
+        }
+    }
+}
---
+++ CaloryAPI/CaloryAPI/Meal.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace CaloryAPI
+{
+    public class Meal
+    {
+       
+    }
+}
